8b531da68b5ca6619030d45e827e05822725e0e5
Slava <lenox1073@gmail.com>
2023-03-06 15:42:42 +0200

    11 commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const hamurger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
-    closeMenu = document.querySelector('.menu__close');
+    closeMenu = document.querySelector('.menu__close')
+    ;
 
 hamurger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -11,9 +12,10 @@ closeMenu.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+
 const counters = document.querySelectorAll('.skills__rating-counter'),
     lines = document.querySelectorAll('.skills__rating-line span');
 
 counters.forEach((items, i) => {
     lines[i].style.width = items.innerHTML;
-})
+});
